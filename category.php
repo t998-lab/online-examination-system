9296639e058cf4ca93ab3b1b2c01615a265df402
Tima Jayyousi <aljaya@gmail.com>
2021-01-09 19:48:58 +0200

    category
    add class category and add CRUD on the file

--- category.php
+++ category.php
@@ -1,20 +1,35 @@
 <?php
+include("category_class.php");
 include("includes/header.php");
+$action = isset($_GET['action']) ? $_GET['action'] : 'dash';
+if ($action == 'dash'){
+    if(isset($_POST['submit']))
+    {
+        $c->cat_name  = $_POST['cat_name'];
+        $c->cat_desc  = $_POST['cat_desc'];
+        if(isset($_FILES['cat_img']['name']))
+        {
+        $c->cat_image = $_FILES['cat_img']['name'];
+        $tmp          = $_FILES['cat_img']['tmp_name'];
+        $path         = 'images/';
+        move_uploaded_file($tmp,$path.$c->cat_image);
+        }
+        $c->create();
+    }
 ?>
-
-<div class="row">
+                        <div class="row">
                            <div class="col-lg-12">
                                 <div class="card">
                                     <div class="card-header">Manage Categories</div>
                                     <div class="card-body">
                                         <div class="card-title">
-                                            <h3 class="text-center title-2">Category Info</h3>
+                                            <h3 class="text-center title-2">Add Category</h3>
                                         </div>
                                         <hr>
                                         <form action="" method="post" enctype="multipart/form-data">
                                             <div class="form-group">
                                                 <label class="control-label mb-1">Category Name</label>
-                                                <input  type="text" class="form-control" name="cat_name">
+                                                <input  type="text" class="form-control" name="cat_name" required>
                                             </div>
                                             <div class="form-group">
                                                 <label class="control-label mb-1">Description</label>
@@ -26,7 +41,7 @@ include("includes/header.php");
                                             </div>
                                             <div>
                                                 <button id="payment-button" type="submit"  name="submit"class="btn btn-lg btn-info btn-block">
-                                                   Save
+                                                   Add
                                                 </button>
                                             </div>
                                         </form>
@@ -35,7 +50,7 @@ include("includes/header.php");
                             </div>
                         </div>
 
- <div class="row m-t-30">
+                      <div class="row m-t-30">
                             <div class="col-md-12">
                                 <!-- DATA TABLE-->
                                 <div class="table-responsive m-b-40">
@@ -52,7 +67,23 @@ include("includes/header.php");
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            
+                                            <?php
+                                            if($c->read()){
+                                            foreach($c->read() as $row)
+                                            {
+                                                echo "<tr><td>{$row['cat_id']}</td><td>{$row['cat_name']}</td><td>{$row['cat_desc']}</td>";
+                                                if($row['cat_image']!=null){
+                                                echo "<td><img src='images/{$row['cat_image']}' width='100px' height='100px'></td>";
+                                                }
+                                                else{
+                                                    echo "<td>{$row['cat_image']}</td>";
+                                                }
+                                                echo "<td><a href='category.php?action=show&id=".$row['cat_id']."' class='btn btn-secondary'>Show</a></td>";
+                                                echo "<td><a href='category.php?action=edit&id=".$row['cat_id']."&&im=".$row['cat_image']."' class='btn btn-primary'>Edit</a></td>";
+                                                echo "<td><a href='category.php?action=delete&id=".$row['cat_id']."' class='btn btn-danger'>Delete</a></td></tr>";
+                                            }
+                                            }
+                                            ?>
                                         </tbody>
                                     </table>
                                 </div>
@@ -60,5 +91,76 @@ include("includes/header.php");
                             </div>
                         </div>
 <?php
+}
+elseif($action=='delete')
+{
+    $id=$_GET['id'];
+    $c->delete($id);
+    ?>
+    <script type="text/javascript">
+       window.location.href = 'category.php';
+    </script>
+    <?php
+}
+elseif($action=='edit')
+{
+    $id=$_GET['id'];
+    $data=$c->readById($id);
+    if(isset($_POST['edit']))
+    {
+        $c->cat_name = $_POST['cat_name'];
+        $c->cat_desc = $_POST['cat_desc'];
+        if(($_FILES['cat_img']['name']))
+        {   
+            $c->cat_image = $_FILES['cat_img']['name'];
+            $tmp          = $_FILES['cat_img']['tmp_name'];
+            $path         = 'images/';
+            move_uploaded_file($tmp,$path.$c->cat_image);
+        }else{
+            $image=$_GET['im'];
+            $c->cat_image =$image;
+        }
+        $c->edit($id);
+        ?>
+        <script type="text/javascript">
+         window.location.href = 'category.php';
+        </script>
+        <?php
+    }
+    ?>
+    <div class="row">
+                           <div class="col-lg-12">
+                                <div class="card">
+                                    <div class="card-header">Manage Categories</div>
+                                    <div class="card-body">
+                                        <div class="card-title">
+                                            <h3 class="text-center title-2">Edit Category</h3>
+                                        </div>
+                                        <hr>
+                                        <form action="" method="post" enctype="multipart/form-data">
+                                            <div class="form-group">
+                                                <label class="control-label mb-1">Category Name</label>
+                                                <input  type="text" class="form-control" name="cat_name" value="<?php echo $data[0]['cat_name'];?>">
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="control-label mb-1">Description</label>
+                                                <input  type="text" class="form-control"  name="cat_desc" value="<?php echo $data[0]['cat_desc'];?>">
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="control-label mb-1">Image</label>
+                                                <input  type="file" class="form-control"  name="cat_img">
+                                            </div>
+                                            <div>
+                                                <button id="payment-button" type="submit"  name="edit" class="btn btn-lg btn-info btn-block">
+                                                   Edit
+                                                </button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+    <?php
+}
 include("includes/footer.html");
 ?>
---
+++ category_class.php
@@ -0,0 +1,41 @@
+<?php
+require("db_connection.php");
+class Category extends dbconnection {
+    public $cat_id;
+    public $cat_name;
+    public $cat_desc;
+    public $cat_image;
+    public function create()
+    {
+        $query  = "INSERT INTO category(cat_name,cat_desc,cat_image)
+                   VALUES('$this->cat_name','$this->cat_desc','$this->cat_image')";
+        $this->performQuery($query);
+    }
+    public function read()
+    {
+        $query  = "SELECT * FROM category";
+        $result = $this->performQuery($query);
+        return    $this->fetchAll($result);
+    }
+    public function readById($id)
+    {
+        $query  = "SELECT * FROM category WHERE cat_id={$id}";
+        $result = $this->performQuery($query);
+        return    $this->fetchAll($result);
+    }
+    public function delete($id)
+    {
+        $query  = "DELETE FROM category WHERE cat_id={$id}";
+        $result = $this->performQuery($query);
+    }
+    public function edit($id)
+    {
+        $query = "UPDATE category SET cat_name  = '$this->cat_name',
+                                      cat_desc  = '$this->cat_desc',
+                                      cat_image = '$this->cat_image'
+                                      WHERE cat_id={$id}" ;
+        $this->performQuery($query);
+    }
+}
+$c = new Category();
+?>
