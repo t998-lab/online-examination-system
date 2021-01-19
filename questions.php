6e4448e42dfba8a0c4ed1f63ed6aa910cb092826
Israa-Elaiwa1 <74402771+Israa-Elaiwa1@users.noreply.github.com>
2021-01-20 00:55:14 +0200

    update questions.php

--- questions.php
+++ questions.php
@@ -1,7 +1,9 @@
 <?php
 include("questionsClass.php");
 include("includes/header.php");
-$exam_id   =  $_GET['id'];
+$exam_id     =  $_GET['id'];
+$exam_mark   =  $x->readMarkById($exam_id);
+$sum = $x->sum($exam_id);
 
 $action = isset($_GET['action']) ? $_GET['action'] : 'dash';
 //main page
@@ -14,8 +16,9 @@ $x->image      =   $_FILES['q_img']['name'];
 $tmpName       =   $_FILES['q_img']['tmp_name'];
 $path          =   'images/';
 move_uploaded_file($tmpName, $path.$x->image);
-$x->exam_id    =$exam_id;
-
+$x->exam_id    =    $exam_id;
+//to check total questions mark with exam's full mark
+if($exam_mark[0]['exam_mark']>=$sum[0]['sum']+$x->mark){
 
 // type and options
 $x->radio =$_POST['radios'];
@@ -24,18 +27,40 @@ if ($x->radio ==  "option1") {
   $x->two      =  $_POST['secondChoice'];
   $x->three    =  $_POST['thirdChoice'];
   $x->four     =  $_POST['fourthChoice'];
-  $x->correct  =  $_POST['select_correct'];
+
+  if ($_POST['select_correct']=='a'){
+    $x->correct      =  $_POST['firstChoice'];
+  }
+  elseif ($_POST['select_correct']=='b'){
+    $x->correct      =  $_POST['secondChoice'];
+  }
+  elseif ($_POST['select_correct']=='c'){
+    $x->correct      =  $_POST['thirdChoice'];
+  }
+    elseif ($_POST['select_correct']=='d'){
+      $x->correct      =  $_POST['fourthChoice'];
+    }
+
   $x->type     = "multiple";
   $x->exam_id    =$exam_id;
-
 }
 else {
   $x->one1     =  $_POST['first'];
   $x->two1     =  $_POST['second'];
-  $x->correct1 =  $_POST['selectTF'];
   $x->type     =  "true or false";
+
+  if ($_POST['selectTF']=='e'){
+    $x->correct1       =  $_POST['first'];
+  }
+  elseif ($_POST['selectTF']=='f'){
+    $x->correct1      =  $_POST['second'];
+  }
 }
 $x->create();
+}
+else{
+ $error = "Cannot add this question!<br>Question marks are now more than exam's full mark!!!";
+}
 }
 ?>
 
@@ -52,6 +77,8 @@ text-align: center;
 </style>
 
 <div class="row">
+
+
                            <div class="col-lg-12">
                                 <div class="card">
                                     <div class="card-header">Manage Qusetions</div>
@@ -118,12 +145,13 @@ text-align: center;
                                          <input type="text" class="form-control" name="fourthChoice">
                                         </div>
 
-                                       <select class="btn btn-outline-secondary" name = "select_correct">
-                                         <option selected disabled value="">Correct Choice</option>
-                                         <option value="<?php?>" class="dropdown-item">First Choice</option>
-                                         <option class="dropdown-item">Second Choice</option>
-                                         <option class="dropdown-item">Third Choice</option>
-                                         <option class="dropdown-item">Fourth Choice</option>
+                                        <label>Correct Choice</label>
+                                       <select class="btn btn-outline-secondary" name = "select_correct" id="select_correct">
+                                         <option value=""> </option>
+                                         <option value="a"class="dropdown-item">First Choice</option>
+                                         <option value="b"class="dropdown-item">Second Choice</option>
+                                         <option value="c"class="dropdown-item">Third Choice</option>
+                                         <option value="d"class="dropdown-item">Fourth Choice</option>
                                        </select>
                                     </div>
                                     <!-- end of multiple choices -->
@@ -144,25 +172,38 @@ text-align: center;
                                       </div>
                                      <input type="text" class="form-control" name="second" >
                                     </div>
-                                    <select class="btn btn-outline-secondary" name="selectTF">
-                                      <option selected disabled >Correct Choice</option>
-                                      <option class="item">T</option>
-                                      <option class="item">F</option>
+                                    <label>Correct Choice</label>
+                                    <select class="btn btn-outline-secondary" name="selectTF" id="selectTF">
+                                      <option value=""> </option>
+                                      <option value="e" class="item">First Choice</option>
+                                      <option value="f"class="item">Second Choice</option>
                                     </select>
                                     </div>
                                     <!-- end of true or false -->
 
                                                 <hr>
-                                                <label>Mark</label>
-                                            <select name="marks" class="custom-select mb-3" required>
-                                              <option selected disabled value="">Enter mark</option>
-                                              <option>1</option>
-                                              <option>2</option>
-                                              <option>3</option>
+                                            <label>Mark</label>
+                                        <select name="marks" class="custom-select mb-3" required>
+                                          <option selected disabled value="">Enter mark</option>
 
-                                            </select>
+                                            <?php
+                                            $option = 1;
+                                            while($option<=$exam_mark[0]['exam_mark'])
+                                            {
+                                              echo"<option>{$option}</option>";
+                                              $option++;
+                                            }
+
+
+                                            ?>
+                                          </select>
+                                            <?php
+                                              if(isset($error)){
+                                                  echo "<div style='background:#FFDDDD;padding:6px;margin-bottom:20px'>".$error."</div>";
+                                              }
+                                             ?>
                                             <div>
-                                                <button id="payment-button" type="submit"  name="submit"class="btn btn-lg btn-info btn-block">
+                                                <button id="payment-button" type="submit"  name="submit"class="btn btn-lg btn-primary btn-block">
                                                    Save
                                                 </button>
                                             </div>
@@ -190,28 +231,25 @@ text-align: center;
                                             <tbody>
                                             <?php
 
-                                            if ($x->readQByExamId($exam_id)) {
-                                            foreach ($x->readQByExamId($exam_id) as $row) {
-                                              $picture="";
+                                            if ($x->readQ($exam_id)) {
+                                            foreach ($x->readQ($exam_id) as $row) {
                                               echo"<tr>";
                                               echo"<td>{$row['q_id']}</td>";
                                                   echo"<td>{$row['q_text']} </td>";
-                                                  
                                                   if($row['q_image']!=null){
-                                                    $picture="{$row['q_image']}";
                                                   echo"<td width ='200px'><img src ='images/{$row['q_image']}'
                                                            height='100' width='100'/> </td>";
                                                   }
                                                    else {
-                                                     echo"<td width ='200px'></td>";
+                                                     echo"<td> </td>";
                                                   }
                                                   $option = $x->readOptionById($row['q_id']);
-                                                  echo"<td>{$option[0]['option1']},{$option[0]['option2']}
-                                                           {$option[0]['option3']},{$option[0]['option4']}
+                                                  echo"<td>{$option[0]['option1']}<br>{$option[0]['option2']}<br>
+                                                           {$option[0]['option3']}<br>{$option[0]['option4']}
                                                        </td>";
                                                   echo"<td>{$row['q_mark']}</td>";
-                                                  echo"<td><a href='editQuestion.php?examId={$exam_id}&&id={$row['q_id']}' class='btn btn-primary'>Edit</td>";
-                                                  echo"<td style='text-align:center'><a href='questions.php?action=delete&examId={$exam_id}&id={$row['q_id']}' class='btn btn-danger'>Delete</a></td>";
+                                                  echo"<td><a href='editQuestion.php?id={$row['q_id']}&examID={$exam_id}' class='btn btn-primary'>Edit</td>";
+                                                  echo"<td style='text-align:center'><a href='questions.php?action=delete&id={$row['q_id']}&examID={$exam_id}' class='btn btn-danger'>Delete</a></td>";
                                               echo"</tr>";
                                             }
                                           }
@@ -226,14 +264,12 @@ text-align: center;
                                 }
                                 elseif($action=='delete') {
                                 $id  = $_GET['id'];
-                                $examID=$_GET['examId'];
-                              
+                                $examID=$_GET['examID'];
                                 $x->delete($id);
-                                echo"<script>window.location='questions.php?id=".$examID."'</script>";
-                                ?>
-                                
+                                echo"<script>window.location ='questions.php?id=$examID';</script>";
 
-<?php
+
+
 }
 include("includes/footer.html");
 ?>
@@ -242,11 +278,15 @@ include("includes/footer.html");
 $("#radio1").click(function(){
     $(".multipleQ").css("display", "inline");
     $(".trueFalse").css("display", "none");
+    $("#selectTF").prop('required',false);
+    $("#select_correct").prop('required',true);
   })
 
   $("#radio2").click(function(){
     $(".multipleQ").css("display", "none");
     $(".trueFalse").css("display", "inline");
+    $("#select_correct").prop('required',false);
+    $("#selectTF").prop('required',true);
   })
 
 </script>
