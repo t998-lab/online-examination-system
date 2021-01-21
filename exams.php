f138204b814bfb734be19795e649304c2ce29b56
Israa-Elaiwa1 <74402771+Israa-Elaiwa1@users.noreply.github.com>
2021-01-21 21:39:50 +0200

    Update exams.php

--- exams.php
+++ exams.php
@@ -1,5 +1,6 @@
 <?php
 include("exam_class.php");
+include_once("questionsClass.php"); //here
 include("includes/header.php");
 $action = isset($_GET['action']) ? $_GET['action'] : 'dash';
 if($action=='dash')
@@ -21,6 +22,7 @@ if($action=='dash')
                                         <div class="card-title">
                                             <h3 class="text-center title-2">Add Exam</h3>
                                         </div>
+
                                         <hr>
                                         <form action="" method="post">
                                             <div class="form-group">
@@ -31,6 +33,7 @@ if($action=='dash')
                                                 <label class="control-label mb-1">Description</label>
                                                 <input  type="text" class="form-control"  name="exam_desc">
                                             </div>
+
                                             <div class="form-group">
                                                 <label class="control-label mb-1">Mark</label>
                                                 <input  type="text" class="form-control"  name="exam_mark">
@@ -47,7 +50,7 @@ if($action=='dash')
                                               ?>
                                             </select>
                                             <div>
-                                                <button id="payment-button" type="submit"  name="submit"class="btn btn-lg btn-info btn-block">
+                                                <button id="payment-button" type="submit"  name="submit"class="btn btn-lg btn-primary btn-block">
                                                    Add
                                                 </button>
                                             </div>
@@ -69,6 +72,9 @@ if($action=='dash')
                                                 <th>description</th>
                                                 <th>Mark</th>
                                                 <th>Category</th>
+                                                <th>examinees</th>
+                                                <th class='text-success'>Passed</th>
+                                                <th class='text-danger'>Failed</th>
                                                 <th>show</th>
                                                 <th>Edit</th>
                                                 <th>Delete</th>
@@ -83,11 +89,14 @@ if($action=='dash')
                                                 $category=$c->readById($cat_id);
                                                 echo "<tr><td>{$row['exam_id']}</td><td>{$row['exam_name']}</td><td>{$row['exam_desc']}</td>";
                                                 echo "<td class='text-primary'>{$row['exam_mark']}</td><td>".$category[0]['cat_name']."</td>";
-                                                echo "<td><a href='exams.php?action=show&id=".$row['exam_id']."' class='btn btn-secondary'>Show</a></td>";
-                                                echo "<td><a href='exams.php?action=edit&id=".$row['exam_id']."' class='btn btn-primary'>Edit</a></td>";
-                                                echo "<td><a href='exams.php?action=delete&id=".$row['exam_id']."' class='btn btn-danger'>Delete</a></td></tr>";
-                                               
-                                                
+                                                echo "<td>{$row['tekenNum']}</td>";
+                                                echo "<td class='text-success'>{$row['pass']}</td>";
+                                                echo "<td class='text-danger'>{$row['fail']}</td>";
+                                                echo "<td><a href='questions.php?id={$row['exam_id']}' class='btn btn-sm btn-secondary'>Show</a></td>";
+                                                echo "<td><a href='exams.php?action=edit&id=".$row['exam_id']."' class='btn btn-sm btn-primary'>Edit</a></td>";
+                                                echo "<td><a href='exams.php?action=delete&id=".$row['exam_id']."' class='btn btn-sm btn-danger'>Delete</a></td></tr>";
+
+
                                             }
                                             }
                                             ?>
@@ -97,6 +106,7 @@ if($action=='dash')
                                 <!-- END DATA TABLE-->
                             </div>
                         </div>
+
 <?php
 }
 elseif($action=='delete')
