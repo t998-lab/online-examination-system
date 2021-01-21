ccbe7983ef691ecc8cb61325263ad96f5bf39ee5
Israa-Elaiwa1 <74402771+Israa-Elaiwa1@users.noreply.github.com>
2021-01-21 21:38:06 +0200

    Update students.php

--- students.php
+++ students.php
@@ -2,19 +2,21 @@
 include("studentClass.php");
 include("includes/header.php");
 $action = isset($_GET['action']) ? $_GET['action'] : 'dash';
-
-
+if($action=='dash')
+{
 ?>
 
                       <div class="top-campaign">
-                                    <h3 class="title-3 m-b-30">Qusetions</h3>
+                                    <h3 class="title-3 m-b-30">Manage students</h3>
                                     <div class="table-responsive">
                                         <table class="table table-top-campaign">
+                                               <th>Student Id</th>
                                                <th>Name</th>
                                                 <th>Email</th>
                                                 <th>Mobile</th>
-                                                <th>IMAGE</th>
+                                                <th>Image</th>
                                                 <th>Education Level</th>
+                                                <th>Edit</th>
                                                 <th>Delete</th>
                                             </tr>
                                         </thead>
@@ -28,21 +30,21 @@ $action = isset($_GET['action']) ? $_GET['action'] : 'dash';
                                                 foreach ($data as $row)
                                                 {
 
-                                                    //echo "<td>{$row['pro_id']}</td>";
                                                     echo "<tr>";
-                                                    //echo"<td>{$row['st_id']}</td>";
+                                                    echo"<td>{$row['st_id']}</td>";
                                                     echo"<td>{$row['st_name']}</td>";
                                                     echo"<td>{$row['st_email']}</td>";
                                                     echo"<td>{$row['st_mobile']}</td>";
                                                     if($row['st_image']!=null)
                                                     {
-                                                    echo"<td><img width='50px' height='50px' src='images/{$row['st_image']}'></td>";
+                                                    echo"<td><img width='75px' height='75px' src='images/{$row['st_image']}'></td>";
                                                     }
                                                     else{
-                                                    echo"<td></td>";     
+                                                    echo"<td></td>";
                                                     }
                                                     echo"<td>{$row['ed_level']}</td>";
-                                                    echo "<th><a href='students.php?action=delete&id={$row['st_id']}' class='btn btn-danger'>Delete</a></th>";
+                                                    echo "<td><a href='students.php?action=edit&id={$row['st_id']}' class='btn btn-primary'>Edit</a></td>";
+                                                    echo "<td><a href='students.php?action=delete&id={$row['st_id']}' class='btn btn-danger'>Delete</a></td>";
                                                     echo "</tr>";
                                                 }
                                             }
@@ -54,6 +56,7 @@ $action = isset($_GET['action']) ? $_GET['action'] : 'dash';
                             </div>
                         </div>
 <?php
+}
 if($action=='delete')
 {$id=$_GET['id'];
 $c->delete($id);
@@ -62,10 +65,64 @@ $c->delete($id);
 window.location.href = 'students.php';
 </script>
 <?php
+}
 
+elseif($action=='edit')
+{
+    $id   = $_GET['id'];
+    $data = $c->readById($id);
+    if(isset($_POST['edit']))
+    {
+        $c->st_name    = $_POST['st_name'];
+        $c->st_email   = $_POST['st_email'];
+        $c->st_mobile  = $_POST['st_mobile'];
+        $c->edit($id);
+        ?>
+            <script type='text/javascript'>
+             window.location.href = 'students.php';
+            </script>
 
+        <?php
+    }
+    ?>
+    <div class="row">
+                           <div class="col-lg-12">
+                                <div class="card">
+                                    <div class="card-header">Manage Students</div>
+                                    <div class="card-body">
+                                        <div class="card-title">
+                                            <h3 class="text-center title-2">Edit Student</h3>
+                                        </div>
+                                        <hr>
+                                        <form action="" method="post">
+                                            <div class="form-group">
+                                                <label class="control-label mb-1">Student Name</label>
+                                                <input  type="text" class="form-control" name="st_name"
+                                                        value="<?php echo $data[0]['st_name'];?>">
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="control-label mb-1">Student Email</label>
+                                                <input  type="text" class="form-control"  name="st_email"
+                                                value="<?php echo $data[0]['st_email'];?>">
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="control-label mb-1">Student Mobile Number</label>
+                                                <input  type="text" class="form-control"  name="st_mobile"
+                                                value="<?php echo $data[0]['st_mobile'];?>">
+                                            </div>
+                                            <div>
+                                                <button id="payment-button" type="submit"  name="edit"class="btn btn-lg btn-info btn-block">
+                                                   Edit Student Information
+                                                </button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+    <?php
 }
 
 include("includes/footer.html");
 
-?> 
+?>
